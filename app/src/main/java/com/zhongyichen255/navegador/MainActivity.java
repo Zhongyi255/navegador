b1eab3fce03060a4124119a46dfd73c1128cb300
zhongyi <zhongyichen255@gmail.com>
2021-06-28 15:18:21 +0200

    Crear proyecto

---
+++ app/src/main/java/com/zhongyichen255/navegador/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zhongyichen255.navegador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
